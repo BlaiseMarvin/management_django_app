28e628f60d52ae5e4bf20c6b84e7028a67d7ae5a
BlaiseMarvin <blaisemarvinrusoke@gmail.com>
2023-06-28 11:21:10 +0300

    created the intern profile model

--- intern_data/models.py
+++ intern_data/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class InternProfile(models.Model):
+    firstname=models.CharField(max_length=255)
+    lastname=models.CharField(max_length=255)
+    email=models.EmailField()
+    age=models.IntegerField()
+    university=models.CharField(max_length=255)
